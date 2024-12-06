19cbb0fd100eb42e78c438970cdd36016ffd89c6
yibeth06 <yibethospina06@gmail.com>
2024-12-06 16:19:13 -0500

    Continua 1

--- src/Punto5.java
+++ src/Punto5.java
@@ -3,11 +3,15 @@ import java.util.Scanner;
 public class Punto5 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Ingrese el total de su compra:");
+
+        System.out.println("Ingresar  el total de su compra:");
+
         double total = scanner.nextDouble();
+
         if (total >= 1000) {
             total *= 0.8; // Descuento del 20%
             System.out.println("El total después del descuento es: " + total);
+
         } else if (total >= 500 && total <1000) {
             total *= 0.9; // Descuento del 10%
             System.out.println("El total después del descuento es: " + total);
